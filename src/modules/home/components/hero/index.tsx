5b2badf71cfe231dae6592c8c5301de6802c23ad
captjay98 <captjay98@gmail.com>
2024-02-22 05:48:46 +0100

    img fix

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -2,7 +2,7 @@ import { Heading } from "@medusajs/ui"
 
 const Hero = () => {
   return (
-    <div className="bg[url(/landing-pic.jpg)] relative w-full border-b h-[70vh] border-ui-border-base">
+    <div className=" bg-[url(/landing-pic.jpg)] relative w-full border-b h-[70vh] border-ui-border-base">
       <div className="bg-[hsla(0,0%,0%,0.5)]  flex absolute inset-0 z-10 flex-col gap-6 justify-center items-center text-center small:p-32">
         <span>
           <Heading
