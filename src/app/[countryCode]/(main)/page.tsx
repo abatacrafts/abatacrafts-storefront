e7191954b3db0f341b3dfded87e8b99ad9c4de16
captjay98 <captjay98@gmail.com>
2024-02-22 06:17:32 +0100

    Meta update

--- src/app/[countryCode]/(main)/page.tsx
+++ src/app/[countryCode]/(main)/page.tsx
@@ -8,9 +8,9 @@ import { ProductCollectionWithPreviews } from "types/global"
 import { cache } from "react"
 
 export const metadata: Metadata = {
-  title: "Medusa Next.js Starter Template",
+  title: "AbataCrafts By Sally",
   description:
-    "A performant frontend ecommerce starter template with Next.js 14 and Medusa.",
+    "AbataCrafts By Sally is a handMade Beads Bags and Jewellery Store.",
 }
 
 const getCollectionsWithProducts = cache(
